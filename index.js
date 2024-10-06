a3c1adabff01c42abbfb227ee83304456d2dbb16
Uma <umas70204@gmail.com>
2024-10-06 14:55:48 +0530

    some parts were updated in the code

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 let arr = [34,55,66,87,2,4,6,11]
 
-let count =0;
-for(let i = 0 ; i<arr.length ; i++){
 
+for(let i = 0 ; i<arr.length ; i++){
+    let count =0;
     for(let j = 1 ; j <= arr[i] ; j++){
         if(arr[i] % j == 0){
             count++;
         }
     }
-}
-if(count == 2){
-    console.log(arr[i] ,"is a prime number ");
+
+    if(count == 2){
+        console.log(arr[i] ,"is a prime number ");
+    }else{
+        console.log(arr[i], "its not a prime number ")
+    }
+    
 }
