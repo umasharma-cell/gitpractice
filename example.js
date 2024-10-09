4fe2286939a90f5b72252e6db62645d97d297992
Uma <umas70204@gmail.com>
2024-10-09 21:04:30 +0530

    Adding two more files

---
+++ example.js
@@ -0,0 +1,7 @@
+// print the pattern
+let str = " ";
+for(let i = 1 ; i<= 5 ; i++){
+
+    str = str + "*" 
+    console.log (str);
+}
---
+++ sample_code.js
@@ -0,0 +1,8 @@
+// print the numbers using while loop
+
+let num = 5;
+let i= 0;
+while(i <= num){
+    console.log(i);
+    i++;
+}
